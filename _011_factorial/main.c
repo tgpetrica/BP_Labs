a049ae0e20446ffd7c932fdb6c3f8e724d07e3df
Teodor <teodor.petrica@rovacc.ro>
2023-12-13 10:32:57 +0200

    add factorial

---
+++ _011_factorial/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+unsigned short factorialRecursiv(unsigned short);
+unsigned short factorialIterativ(unsigned short);
+
+void main()
+{
+    unsigned short numar;
+    printf("Introduceti un numar: ");
+    scanf("%hi", &numar);
+
+    printf("Factorialul numarului %hi este %hi (recursiv)\n", numar, factorialRecursiv(numar));
+    printf("Factorialul numarului %hi este %hi (iterativ)\n", numar, factorialIterativ(numar));
+}
+
+unsigned short factorialRecursiv(unsigned short numar)
+{
+    if (numar == 0)
+        return 1;
+    else
+        return numar * factorialRecursiv(numar - 1);
+}
+
+unsigned short factorialIterativ(unsigned short numar)
+{
+    int factorial = 1;
+    for (int i = 1; i <= numar; i++)
+        factorial *= i;
+    return factorial;
+}
